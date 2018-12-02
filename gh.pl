1676a69532273a81a15e31a7221cadf30ab885d1
Shristikedia <39804023+Shristikedia@users.noreply.github.com>
2018-12-02 11:07:14 +0530

    Update gh.pl

--- gh.pl
+++ gh.pl
@@ -197,6 +197,151 @@ packet(X):- (check(X), (allow_due_to_adapter(X); reject(X);drop(X);allow(X))).
 %packet consition
 %
 
+isl_ipv6([1,2,3,4,5,6,7,8]).
+isl2_ipv6([any]).
+
+check_Adapter_ipv6(X):-   isl_ipv6(L),isl2_ipv6(L2),not(member(X,L)),not(member(X,L2)),write('not correct adapter format').
+check_Src_ipv6(X):-       isl2_ipv6(L2),not(member(X,L2)),pow2(2,128,Z),not(between(1,Z,X)),write('not correct src address').
+check_Dest_ipv6(X):-      isl2_ipv6(L2),not(member(X,L2)),pow2(2,128,Z),not(between(1,Z,X)),write('not correct dest address').
+check_Port_ipv6(X):-      isl2_ipv6(L2),not(member(X,L2)),not(between(0,65535,X)),write('not correct port no').
+check_Proto_ipv6(X):-     isl2_ipv6(L2),not(member(X,L2)),not(between(1,255,X)),write('not correct protocol no').
+check_Vlan_ipv6(X):-      isl2_ipv6(L2),not(member(X,L2)),not(between(1,4094,X)),write('not correct vlan id').
+check_Icmp_Type_ipv6(X):- isl2_ipv6(L2),not(member(X,L2)),not(between(0,254,X)),write('not correct icmp type').
+check_Icmp_Code_ipv6(X):- isl2_ipv6(L2),not(member(X,L2)),not(between(0,15,X)),write('not correct icmp code').
+check_ipv6(List):-        pop(List,AdapterNo,L1),not(check_Adapter_ipv6(AdapterNo)),pop(L1,SrcAddress,L2),not(check_Src_ipv6(SrcAddress)),pop(L2,DestAddress,L3),not(check_Dest_ipv6(DestAddress)),pop(L3,PortNo,L4),not(check_Port_ipv6(PortNo)),pop(L4,PortNo1,L5),not(check_Port_ipv6(PortNo1)),pop(L5,ProtoNo,L6),not(check_Proto_ipv6(ProtoNo)),pop(L6,VlanId,L7),not(check_Vlan_ipv6(VlanId)),pop(L7,IcmpType,L8),not(check_Icmp_Type_ipv6(IcmpType)),pop(L8,IcmpCode,L9),not(check_Icmp_Code_ipv6(IcmpCode)).
+
+
+src_port_droplist_ipv6([12322,53241]).
+src_port_drop_range_ipv6(X):- (X>=45),(X =<90).
+% Block if port is in range or src address or destination address in range.
+
+dest_port_droplist_ipv6([21,4,324,432]).
+dest_port_drop_range_ipv6(X) :- (X>= 22),(X=< 435).
+
+src_ip_drop_list_ipv6([6,9,11,13]).
+dst_ip_drop_list_ipv6([19,12,45,66]).
+range_src_ip_drop_ipv6(X):-(X>=56),(X=<78).
+range_dst_ip_drop_ipv6(X):-(X>=100),(X=<200).
+
+ether_vlan_id_droplist_ipv6([423,55,21]).
+ether_vlan_id_drop_range_ipv6(X):- (X>= 400, X=< 500).
+ether_vlan_id_drop_ipv6(X):- (ether_vlan_id_droplist_ipv6(L),member(X,L));(ether_vlan_id_drop_range_ipv6(X)).
+
+proto_allow_list_ipv6([1,6,17]).
+
+
+src_ip_drop_ipv6(X):-(src_ip_drop_list_ipv6(L1),member(X,L1));range_src_ip_drop_ipv6(X).
+
+dst_ip_drop_ipv6(X):-(dst_ip_drop_list_ipv6(L2),member(X,L2));range_dst_ip_drop_ipv6(X).
+
+src_port_drop_ipv6(X) :- (src_port_droplist_ipv6(L),member(X,L));src_port_drop_range_ipv6(X).
+
+dest_port_drop_ipv6(X):- (dest_port_droplist_ipv6(M),member(X,M));dest_port_drop_range_ipv6(X).
+
+proto_drop_ipv6(X):-not((any_list(R2),member(X,R2));(proto_allow_list_ipv6(R1),member(X,R1))).
+
+drop_ipv6(X):-(
+
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,D,L3),pop(L3,Po,L4),pop(L4,P1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9),src_ip_drop_ipv6(SrcAddress));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,Po,L4),pop(L4,P1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9),dst_ip_drop_ipv6(DestAddress));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,PortNo,L4),pop(L4,P1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9),src_port_drop_ipv6(PortNo));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,PortNo,L4),pop(L4,PortNo1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9), dest_port_drop_ipv6(PortNo1));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,PortNo,L4),pop(L4,PortNo1,L5),pop(L5,ProtoNo,L6), proto_drop_ipv6(ProtoNo));
+
+             (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,PortNo,L4),pop(L4,PortNo1,L5),pop(L5,ProtoNo,L6), pop(L6,vlanid,L7),ether_vlan_id_drop_ipv6(vlanid))),write('packet is dropped(silently) due to ipv4.').
+
+
+
+
+src_port_rejectlist_ipv6([331,56,86,231]).
+dest_port_rejectlist_ipv6([674,344,7867]).
+
+src_ip_reject_list_ipv6([1,4]).
+dst_ip_reject_list_ipv6([129,212]).
+
+src_port_reject_range_ipv6(X):- (X>=21),(X=< 78).
+dest_port_reject_range_ipv6(X) :- (X>= 22),(X=< 435).
+range_src_ip_reject_ipv6(X):-(X>=158),(X=<178).
+range_dst_ip_reject_ipv6(X):-(X>=320),(X=<360).
+icmp_type_reject_list_ipv6([1,312,5]).
+%icmp_code_reject_list([1,3,2]).
+
+src_port_reject1_ipv6(X) :- ((src_port_rejectlist_ipv6(N),member(X,N));src_port_reject_range_ipv6(X)),write('rejected due to source port due to ipv6').
+dest_port_reject1_ipv6(X):-((dest_port_rejectlist_ipv6(O),member(X,O));dest_port_reject_range_ipv6(X)),write('rejected due to destination port due to ipv6').
+
+src_ip_reject1_ipv6(X):-((src_ip_reject_list_ipv6(P1),member(X,P1));range_src_ip_reject_ipv6(X)),write('rejected due to source address due to ipv6').
+dst_ip_reject1_ipv6(X):-((dst_ip_reject_list_ipv6(P2),member(X,P2));range_dst_ip_reject_ipv6(X)),write('rejected due to destination address due to ipv6').
+
+
+src_port_reject_ipv6(X,Y,Z) :- ((src_port_rejectlist_ipv6(N),member(X,N)); src_port_reject_range_ipv6(X)),write('rejected due to source port due to ipv6'),((Y=:=0)->write('ICMP Type not declared');(write('ICMP Type:'),write( Y ),write(' ICMP Code:'), write(Z))).
+dest_port_reject_ipv6(X,Y,Z):- ((dest_port_rejectlist_ipv6(O),member(X,O));dest_port_reject_range_ipv6(X)) ,write('rejected due to destination port considering ipv4'),((Y=:=0)->write('ICMP Type not declared');(write('ICMP Type:'),write(Y),write(' ICMP Code:'),write(Z))).
+src_ip_reject_ipv6(X,Y,Z):-((src_ip_reject_list_ipv6(P1),member(X,P1));range_src_ip_reject_ipv6(X)),write('rejected due to source address considering ipv4 '),((Y=:=0)->write('ICMP Type not declared');(write('ICMP Type:'),write(Y),write( ' ICMP Code:'),write(Z))).
+dst_ip_reject_ipv6(X,Y,Z):-((dst_ip_reject_list_ipv6(P2),member(X,P2));range_dst_ip_reject_ipv6(X)),write('rejected due to destination address considering ipv4'),((Y=:=0)->write('ICMP Type not declared');(write('ICMP Type:'),write(Y),write(' ICMP Code:'),write(Z))).
+
+icmp_reject_ipv6(X,Y,Z):- (icmp_type_reject_list_ipv6(L),member(X,L)),write('rejected due to icmp type considering ipv4'),((Y=:=0)->write('ICMP Type not declared');(write('ICMP Type:'),write(Y),write(' ICMP Code:'),write(Z))).
+
+
+
+reject_ipv6(X):-(
+                (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,D,L3),pop(L3,Po,L4),pop(L4,P1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9), icmp_reject_ipv6(It,It,Ic));
+
+    (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,D,L3),pop(L3,Po,L4),pop(L4,P1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9),src_ip_reject_ipv6(SrcAddress,It,Ic));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,Po,L4),pop(L4,P1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9),dst_ip_reject_ipv6(DestAddress,It,Ic));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,PortNo,L4),pop(L4,P1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9),src_port_reject_ipv6(PortNo,It,Ic));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,PortNo,L4),pop(L4,PortNo1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9), dest_port_reject_ipv6(PortNo1,It,Ic))
+           ).
+
+
+ether_vlan_id_allowlist_ipv6([423,55,21]).
+ether_vlan_id_allow_range_ipv6(X):- (X>= 400, X=< 500).
+ether_vlan_id_allow_ipv6(X):- (ether_vlan_id_allowlist_ipv6(L),member(X,L));(ether_vlan_id_allow_range_ipv6(X)).
+
+
+
+proto_allow_ipv6(X):-(any_list(R2),member(X,R2));(proto_allow_list_ipv6(R1),member(X,R1)).
+
+
+src_ip_allow_ipv6(X):-(any_list(Q),member(X,Q));(not(src_ip_drop_ipv6(X)),not(src_ip_reject1_ipv6(X))).
+dst_ip_allow_ipv6(X):-(any_list(Q),member(X,Q));(not(dst_ip_drop_ipv6(X)),not(dst_ip_reject1_ipv6(X))).
+
+any_list_ipv6([any]).
+src_port_allow_ipv6(X):-(any_list_ipv6(P),member(X,P));(not(src_port_drop_ipv6(X)),not(src_port_reject1_ipv6(X))).
+
+
+dest_port_allow_ipv6(X):- (any_list(Q),member(X,Q));(not(dest_port_drop_ipv6(X)),not(dest_port_reject1_ipv6(X))).
+
+allow_ipv6(X):-(
+
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,D,L3),pop(L3,Po,L4),pop(L4,P1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9),src_ip_allow_ipv6(SrcAddress));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,Po,L4),pop(L4,P1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9),dst_ip_allow_ipv6(DestAddress));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,PortNo,L4),pop(L4,P1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9),src_port_allow_ipv6(PortNo));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,PortNo,L4),pop(L4,PortNo1,L5),pop(L5,P,L6),pop(L6,V,L7),pop(L7,It,L8),pop(L8,Ic,L9), dest_port_allow_ipv6(PortNo1));
+
+               (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,PortNo,L4),pop(L4,PortNo1,L5),pop(L5,ProtoNo,L6), proto_allow_ipv6(ProtoNo));
+
+             (pop(X,AdapterNo,L1),pop(L1,SrcAddress,L2),pop(L2,DestAddress,L3),pop(L3,PortNo,L4),pop(L4,PortNo1,L5),pop(L5,ProtoNo,L6), pop(L6,vlanid,L7),ether_vlan_id_allow_ipv6(vlanid))),write('packet is allowed due to ipv4.').
+
+
+adaptlist_ipv6([1,2,any]).
+
+allow_due_to_adapter_ipv6(X) :-  (pop(X,AdapterNo,L1),adaptlist_ipv6(K),not(member(X,K))), write('allowed directly as adapter not doesnot match list of adapters').
+
+
+
+
+packet_ipv6(X):- (check_ipv6(X), (allow_due_to_adapter_ipv6(X); reject_ipv6(X);drop_ipv6(X);allow_ipv6(X))).
 
 
 
